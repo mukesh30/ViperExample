c56908489e62228105d687ca36ea5e1e4b90898b
apple <apple@apples-Mac-mini.local>
2017-12-23 12:44:11 +0530

    Initial Commit

---
+++ ViperExample/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  ViperExample
+//
+//  Created by apple on 23/12/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+import UIKit
+struct ColorOptions {
+    struct HeaderColorOptions {
+        static let  USERPROFILEHEADERCOLOR = UIColor(rgba: "#FFEDB4")
+        
+    }
+    struct ButtonHeightWeightOption {
+        
+        
+    }
+    
+}
